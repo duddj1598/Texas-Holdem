57450f0827ca25195b803a4c1012d16887fc6541
juno <hellojuno6649@gmail.com>
2026-06-16 22:24:38 +0900

    react 중복 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { motion, AnimatePresence } from 'framer-motion';
 
